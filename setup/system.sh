a4eafcb2967d7d6e37c7c752185926ffad505dec
cuiweisong01 <cuiweisong01@MacBook-Pro-4.local>
2025-05-16 10:35:54 +0800

    remove stupid firewall by ufw

--- setup/system.sh
+++ setup/system.sh
@@ -111,15 +111,15 @@ hide_output add-apt-repository --y ppa:ondrej/php
 # of things from Ubuntu, as well as the directory of packages provide by the
 # PPAs so we can install those packages later.
 
-echo "Updating system packages..."
-hide_output apt-get update
-apt_get_quiet upgrade
+#echo "Updating system packages..."
+#hide_output apt-get update
+#apt_get_quiet upgrade
 
 # Old kernels pile up over time and take up a lot of disk space, and because of Mail-in-a-Box
 # changes there may be other packages that are no longer needed. Clear out anything apt knows
 # is safe to delete.
 
-apt_get_quiet autoremove
+#apt_get_quiet autoremove
 
 # ### Install System Packages
 
@@ -255,33 +255,6 @@ APT::Periodic::Unattended-Upgrade "1";
 APT::Periodic::Verbose "0";
 EOF
 
-# ### Firewall
-
-# Various virtualized environments like Docker and some VPSs don't provide #NODOC
-# a kernel that supports iptables. To avoid error-like output in these cases, #NODOC
-# we skip this if the user sets DISABLE_FIREWALL=1. #NODOC
-if [ -z "${DISABLE_FIREWALL:-}" ]; then
-	# Install `ufw` which provides a simple firewall configuration.
-	apt_install ufw
-
-	# Allow incoming connections to SSH.
-	ufw_limit ssh;
-
-	# ssh might be running on an alternate port. Use sshd -T to dump sshd's #NODOC
-	# settings, find the port it is supposedly running on, and open that port #NODOC
-	# too. #NODOC
-	SSH_PORT=$(sshd -T 2>/dev/null | grep "^port " | sed "s/port //" | tr '\n' ' ') #NODOC
-	if [ -n "$SSH_PORT" ]; then
-	    for port in $SSH_PORT; do
-	        if [ "$port" != "22" ]; then
-	            echo "Opening alternate SSH port $port." #NODOC
-                ufw_limit "$port" #NODOC
-            fi
-        done
-	fi
-
-	ufw --force enable;
-fi #NODOC
 
 # ### Local DNS Service
 
